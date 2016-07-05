ae2c5fdce2f1b79fb1f7380d839998c291b66135
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-07-05 21:29:42 +0200

    No more app limit, thanks to 3.2.0

--- tools/limit/10.sh
+++
@@ -1,2 +0,0 @@
-#!/sbin/sh
-sed -i 's:ro.sys.fw.bg_apps_limit=.*:ro.sys.fw.bg_apps_limit=10:' /system/build.prop
--- tools/limit/30.sh
+++
@@ -1,2 +0,0 @@
-#!/sbin/sh
-sed -i 's:ro.sys.fw.bg_apps_limit=.*:ro.sys.fw.bg_apps_limit=30:' /system/build.prop
