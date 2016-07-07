7d4a83faa6e7388fe6a79b8aed8a861858156b2f
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-07-07 17:13:52 +0200

    build.sh : add oos 3.2.1

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Author : Nevax
 
 VERSION=0
-OOS=OnePlus3Oxygen_16_OTA_010_all_1607012342_741e629725ed457b
+OOS=OnePlus3Oxygen_16_OTA_011_all_1607052050_0e5983ace5314161
 SU=UPDATE-SuperSU-v2.76-20160630161323
 XPOSED_APK=XposedInstaller_3.0_alpha4
 DIVINE=DiVINE_BEATS_v7.0_EVOLUTION_BY_THE_ROYAL_SEEKER
@@ -56,7 +56,7 @@ then
 else
    echo "File $OOS.zip does not exist" >&2
    echo "Downloading.."
-   curl -o download/$OOS.zip "https://s3.amazonaws.com/oxygenos.oneplus.net/$OOS.zip"
+   curl -o download/$OOS.zip "http://otafs.coloros.com/patch/amazone2/GLO/OnePlus3Oxygen/OnePlus3Oxygen_16.A.11_GLO_011_1607052050/$OOS.zip"
    echo ""
    echo "testing zip integrity"
    zip -T download/$OOS.zip
