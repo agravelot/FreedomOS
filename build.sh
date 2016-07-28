07c8d462ec1ba45d76c3453268504b16e26713c7
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-07-28 21:37:26 +0200

    New build process
    Support of multiple devices and settings with ystem.new.dat extraction

--- build.sh
+++ build.sh
@@ -3,37 +3,49 @@
 # Author : Nevax
 # Contributor : TimVNL
 
-VERSION="0"
-URLOOS=https://s3.amazonaws.com/oxygenos.oneplus.net
-OOS=OnePlus3Oxygen_16_OTA_012_all_1607251520_5926eed75c18470f
-SU=UPDATE-SuperSU-v2.76-20160630161323
-XPOSED_APK=XposedInstaller_3.0_alpha4
-DIVINE=DiVINE_BEATS_v7.0_EVOLUTION_BY_THE_ROYAL_SEEKER
-DEVICE=op3
+VERSION=0
+DEVICE=0
 MENU=0
+DEVICEMENU=0
 
 echo "#################################"
 echo "FreedomOS build script by Nevax"
 echo "#################################"
 echo ""
-echo "Choose the build method you want:"
-echo "1) Signed (Slower)"
-echo "2) Unsigned (Faster)"
+echo "Available devices:"
+find . -print | grep -i 'device/.*[.]fos'
 echo ""
-read -p "enter build method [2]: " MENU
+read -p "Enter your device codename: " DEVICE
+echo ""
 
-if [ "$MENU" = 1 ];
+if [ -f device/$DEVICE/$DEVICE.fos ];
 then
-        echo "Signed method selected"
-elif  [ "$MENU" = 2 ];
-then
-        echo "Unsigned method selected"
+        source device/$DEVICE/$DEVICE.fos
 else
-        MENU=2
-        echo "Unsigned method selected"
+        echo "Can not find $DEVICE.fos file!"
+        exit
+fi
+
+
+echo ""
+echo "Choose the build method you want:"
+echo "1) user-realease"
+echo "2) debug"
+echo ""
+read -p "enter build method [2]: " BUILD
+
+if [ "$BUILD" = 1 ];
+then
+        echo "user-realease selected"
+elif  [ "$BUILD" = 2 ];
+then
+        echo "debug selected"
+else
+        BUILD=2
+        echo "debug selected"
 fi
 echo ""
-read -p "Enter a version number you want to use [test] : " VERSION
+read -p "Enter the version number [test] : " VERSION
 
 if [ -z "$VERSION" ];
 then
@@ -42,89 +54,76 @@ fi
 
 echo ""
 echo "Clear tmp/ foler..."
-rm -rf tmp/*
+rm -rvf tmp/*
 touch tmp/EMPTY_DIRECTORY
 
 echo ""
 echo "Clear output/ foler..."
-rm -rf output/*.zip
-rm -rf output/*.md5
+rm -rvf output/*.zip
+rm -rvf output/*.md5
+rm -rvf output/patch*
 
 echo ""
 echo "Checking dependencies..."
 echo ""
-if [ -f "download/$OOS.zip" ];
+if [ -f "download/$ROM_NAME.zip" ];
 then
-   echo "File $OOS.zip exist."
+   echo "File $ROM_NAME.zip exist."
 else
-   echo "File $OOS.zip does not exist" >&2
+   echo "File $ROM_NAME.zip does not exist" >&2
    echo "Downloading.."
-   curl -o download/$OOS.zip "$URLOOS/$OOS.zip"
-   echo ""
-   echo "testing zip integrity"
-   zip -T download/$OOS.zip
-   echo ""
-   echo "Clean system/"
-   rm -rf system/*
-   touch "system/EMPTY_DIRECTORY"
-   echo ""
-   echo "Extracting system"
-   unzip -o download/$OOS.zip -d system/
-   echo "Done!"
+   curl -o download/$ROM_NAME.zip $ROM_LINK
 fi
 echo ""
 
-if [ -f "download/$SU.zip" ];
+if [ -d "rom/$DEVICE/" ];
 then
-   echo "File $SU.zip exist."
+  echo "rom/$DEVICE/ dir exist."
 else
-   echo "File $SU.zip does not exist" >&2
-   echo "Downloading.."
-   curl -o download/$SU.zip "http://fr1.androidfilehost.com/dl/1JCF2741XshSLSFUOQPtaQ/1467951889/24591000424944637/$SU.zip"
-   echo ""
-   echo "testing zip integrity"
-   zip -T download/$SU.zip
-   echo "Done!"
-fi
-echo ""
-
-if [ -f "download/$XPOSED_APK.apk" ];
-then
-   echo "File $XPOSED_APK.apk exist."
-else
-   echo "File $XPOSED_APK.apk does not exist" >&2
-   echo "Downloading.."
-   curl -o download/$XPOSED_APK.apk "http://forum.xda-developers.com/attachment.php?attachmentid=3383776&d=1435601440"
-   echo "Done!"
-fi
-echo""
-
-if [ -f "download/$DIVINE.zip" ];
-then
-   echo "File $DIVINE.zip exist."
-else
-   echo "File $DIVINE.zip does not exist" >&2
-   echo "Downloading.."
-   curl -o download/$DIVINE.zip "http://fr1.androidfilehost.com/dl/b-p7sG3YlA4BZN8XoW7tbQ/1467379312/24533103863141857/$DIVINE.zip"
-   echo ""
-   echo "testing zip integrity"
-   zip -T download/$DIVINE.zip
-   echo "Done!"
+  echo ""
+  echo "testing zip integrity"
+  #MD5 instead !!
+  zip -T download/$ROM_NAME.zip
+  echo ""
+  echo "Clean system/"
+  rm -rvf rom/$DEVICE/*
+  touch "system/EMPTY_DIRECTORY"
+  echo ""
+  echo "Extracting system"
+  unzip -o download/$ROM_NAME.zip -d rom/$DEVICE/
+  echo Done!
 fi
 
-echo ""
-#echo "testing downloaded zip integrity"
-#zip -T download/*.zip
+echo "Updating sdat2img tools"
+curl -o download/sdat2img.py https://raw.githubusercontent.com/xpirt/sdat2img/master/sdat2img.py
 
 echo ""
-echo "Copy OOS"
-cp -rvf system/* tmp/
+echo "Copy $ROM_NAME files:"
+cp -rvf rom/$DEVICE/* tmp/
+mkdir tmp/mount
+mkdir tmp/system
+echo ""
+echo "Extracting system.new.dat:"
+download/sdat2img.py tmp/system.transfer.list tmp/system.new.dat tmp/system.img
+echo ""
+echo "Mounting system.new.dat:"
+mount -t ext4 -o loop tmp/system.img tmp/mount/
+echo ""
+echo "Extracting system files:"
+cp -rvf tmp/mount/* tmp/system/
+echo ""
+echo "Clean tmp/"
+umount tmp/mount/
+rm -rvf tmp/mount
+rm -rvf tmp/system.*
+
 echo ""
 echo "Remove META-INF"
-rm -rf "tmp/META-INF"
+rm -rvf "tmp/META-INF"
 echo ""
 echo "Add aroma"
-cp -vR "aroma/META-INF" "tmp/"
+mkdir -p tmp/META-INF/com/google/android/
+cp -vR device/$DEVICE/aroma/* tmp/META-INF/com/google/android/
 echo ""
 echo "Add tools"
 cp -vR "tools" "tmp/"
@@ -132,16 +131,15 @@ echo ""
 echo "Add SuperSU"
 cp -v download/$SU.zip tmp/tools/su/su.zip
 echo ""
-echo "Add xposed apk"
-cp -v download/$XPOSED_APK.apk tmp/tools/xposed/
-echo ""
-echo "Add wallpaper by badboy47"
+echo "Add FreedomOS wallpapers by badboy47"
 mkdir -p tmp/media/wallpaper
 cp -v media/wallpaper/* tmp/media/wallpaper
 echo ""
 echo "Add Divine"
 unzip -o "download/$DIVINE.zip" -d "tmp/tools/divine/"
 echo ""
+
+
 echo "Set version in aroma"
 sed -i.bak "s:!version!:$VERSION:" tmp/META-INF/com/google/android/aroma-config
 echo ""
@@ -153,67 +151,81 @@ sed -i.bak "s:!date!:$(date +"%d%m%y"):" tmp/META-INF/com/google/android/aroma/l
 echo ""
 echo "Set date in fr.lang"
 sed -i.bak "s:!date!:$(date +"%d%m%y"):" tmp/META-INF/com/google/android/aroma/langs/fr.lang
-rm -rf tmp/META-INF/com/google/android/aroma-config.bak
-rm -rf tmp/META-INF/com/google/android/aroma/langs/*.lang.bak
+rm -rvf tmp/META-INF/com/google/android/aroma-config.bak
+rm -rvf tmp/META-INF/com/google/android/aroma/langs/*.lang.bak
 
-
-if [ "$MENU" = 1 ];
+if [ "$BUILD" = 1 ];
 then
   cd tmp/
   echo ""
   echo "Making zip file"
-  zip -r9 "FreedomOS-$DEVICE-nevax-$VERSION.zip" * -x "*EMPTY_DIRECTORY*"
+  zip -r9 "FreedomOS-$CODENAME-nevax-$VERSION.zip" * -x "*EMPTY_DIRECTORY*"
   echo "----"
   cd ..
   echo ""
   echo "Copy Unsigned in output folder"
-  cp -v tmp/FreedomOS-$DEVICE-nevax-$VERSION.zip output/FreedomOS-$DEVICE-nevax-$VERSION.zip
+  cp -v tmp/FreedomOS-$CODENAME-nevax-$VERSION.zip output/FreedomOS-$CODENAME-nevax-$VERSION.zip
   echo ""
   echo "testing zip integrity"
-  zip -T output/FreedomOS-$DEVICE-nevax-$VERSION.zip
+  zip -T output/FreedomOS-$CODENAME-nevax-$VERSION.zip
   echo ""
   echo "Generating md5 hash"
-  openssl md5 "output/FreedomOS-$DEVICE-nevax-$VERSION.zip" |cut -f 2 -d " " > "output/FreedomOS-$DEVICE-nevax-$VERSION.zip.md5"
+  openssl md5 "output/FreedomOS-$CODENAME-nevax-$VERSION.zip" |cut -f 2 -d " " > "output/FreedomOS-$CODENAME-nevax-$VERSION.zip.md5"
   echo ""
   echo "SignApk....."
-  java -jar "SignApk/signapk.jar" "SignApk/testkey.x509.pem" "SignApk/testkey.pk8" "tmp/FreedomOS-$DEVICE-nevax-$VERSION.zip" "tmp/FreedomOS-$DEVICE-nevax-$VERSION-signed.zip"
+  java -jar "SignApk/signapk.jar" "SignApk/testkey.x509.pem" "SignApk/testkey.pk8" "tmp/FreedomOS-$CODENAME-nevax-$VERSION.zip" "tmp/FreedomOS-$DEVICE-nevax-$VERSION-signed.zip"
   echo ""
   echo "Move signed zip file in output folder"
-  mv "tmp/FreedomOS-$DEVICE-nevax-$VERSION-signed.zip" "output/"
-  echo ""
-  echo "testing zip integrity"
-  zip -T "output/FreedomOS-$DEVICE-nevax-$VERSION-signed.zip"
+  mv -v "tmp/FreedomOS-$CODENAME-nevax-$VERSION-signed.zip" "output/"
   echo ""
   echo "Generating md5 hash"
-  openssl md5 "output/FreedomOS-$DEVICE-nevax-$VERSION-signed.zip" |cut -f 2 -d " " > "output/FreedomOS-$DEVICE-nevax-$VERSION-signed.zip.md5"
+  openssl md5 "output/FreedomOS-$CODENAME-nevax-$VERSION-signed.zip" |cut -f 2 -d " " > "output/FreedomOS-$CODENAME-nevax-$VERSION-signed.zip.md5"
   #We doesn't test the final, because it doesn't work with the signed zip.
 fi
 
-if [ "$MENU" = 2 ];
+if [ "$BUILD" = 2 ];
 then
   cd tmp/
   echo ""
   echo "Making zip file"
-  zip -r1 "FreedomOS-$DEVICE-nevax-$VERSION.zip" * -x "*EMPTY_DIRECTORY*"
+  zip -r1 "FreedomOS-$CODENAME-nevax-$VERSION.zip" * -x "*EMPTY_DIRECTORY*"
   echo "----"
   echo ""
   echo "testing zip integrity"
-  zip -T "FreedomOS-$DEVICE-nevax-$VERSION.zip"
+  zip -T "FreedomOS-$CODENAME-nevax-$VERSION.zip"
   echo ""
   cd ..
   echo "Move unsigned zip file in output folder"
-  mv "tmp/FreedomOS-$DEVICE-nevax-$VERSION.zip" "output/"
+  mv -v "tmp/FreedomOS-$CODENAME-nevax-$VERSION.zip" "output/"
   echo ""
   echo "Generating md5 hash"
-  openssl md5 "output/FreedomOS-$DEVICE-nevax-$VERSION.zip" |cut -f 2 -d " " > "output/FreedomOS-$DEVICE-nevax-$VERSION.zip.md5"
-  echo ""
-  echo "testing final zip integrity"
-  zip -T output/FreedomOS-$DEVICE-nevax-$VERSION*.zip
+  openssl md5 "output/FreedomOS-$CODENAME-nevax-$VERSION.zip" |cut -f 2 -d " " > "output/FreedomOS-$CODENAME-nevax-$VERSION.zip.md5"
 fi
 
 echo ""
 echo "Clear tmp/ foler..."
-rm -rf "tmp/*"
+rm -rvf tmp/*
 touch "tmp/EMPTY_DIRECTORY"
 echo ""
 echo "Finish! You can find the build here: output/FreedomOS-$DEVICE-nevax-$VERSION.zip"
+
+
+if [ -d "device/$DEVICE/patch" ];
+then
+  echo ""
+  echo "Building patch file"
+  mkdir -p tmp/patch/
+  cp -rvf device/$DEVICE/patch/ tmp/patch/
+  cd tmp/patch/
+  zip -r9 "patch-FreedomOS-$VERSION.zip" * -x "*EMPTY_DIRECTORY*"
+  cd ../..
+  echo ""
+  echo "Move patch file:"
+  mv -vf tmp/patch/patch-FreedomOS-$VERSION.zip output/patch-FreedomOS-$VERSION.zip
+  echo ""
+  echo "Clear tmp/ foler..."
+  rm -rvf tmp/*
+  touch "tmp/EMPTY_DIRECTORY"
+  echo ""
+  echo "Finish! You can find the build here: output/FreedomOS-$DEVICE-nevax-$VERSION.zip"
+fi
