ea81d3a749a9301392162cb23037b4b39fc42259
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-01 19:37:28 +0200

    Add cleans apps in data

---
+++ tools/clean_apps.sh
@@ -0,0 +1,17 @@
+#!/sbin/sh
+#
+# Author : Nevax
+#
+rm -R /data/app/com.android.chrome-*
+rm -R /data/app/com.google.android.apps.docs-*
+rm -R /data/app/com.google.android.apps.maps-*
+rm -R /data/app/com.google.android.apps.messaging-*
+rm -R /data/app/com.google.android.apps.photos-*
+rm -R /data/app/com.google.android.calendar-*
+rm -R /data/app/com.google.android.gm-*
+rm -R /data/app/com.google.android.googlequicksearchbox-*
+rm -R /data/app/com.google.android.music-*
+rm -R /data/app/com.google.android.talk-*
+rm -R /data/app/com.google.android.youtube-*
+rm -R /data/app/com.touchtype.swiftkey-*
+rm -R /data/app/de.robv.android.xposed.installer-*
