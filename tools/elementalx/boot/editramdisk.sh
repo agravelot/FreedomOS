289067a965b346a5a453f2927cd4d20db58b723e
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-03 04:17:04 +0200

    Add ElementaryX 0.09

---
+++ tools/elementalx/boot/editramdisk.sh
@@ -0,0 +1,29 @@
+#!/sbin/sh
+
+mkdir /tmp/ramdisk
+cp /tmp/boot.img-ramdisk.gz /tmp/ramdisk/
+cd /tmp/ramdisk/
+gunzip -c /tmp/ramdisk/boot.img-ramdisk.gz | cpio -i
+rm /tmp/ramdisk/boot.img-ramdisk.gz
+rm /tmp/boot.img-ramdisk.gz
+
+#Don't force encryption
+if  grep -qr forceencrypt /tmp/ramdisk/fstab.qcom; then
+   sed -i "s/forceencrypt/encryptable/" /tmp/ramdisk/fstab.qcom
+fi
+
+#Start elementalx script
+if [ $(grep -c "import /init.elementalx.rc" /tmp/ramdisk/init.rc) == 0 ]; then
+   sed -i "/import \/init\.environ\.rc/aimport /init.elementalx.rc" /tmp/ramdisk/init.rc
+fi
+
+#Copy wifi module to ramdisk
+cp /tmp/wlan.ko /tmp/ramdisk/sbin/wlan.ko
+chmod 0644 /tmp/ramdisk/sbin/wlan.ko
+
+#copy elementalx scripts
+cp /tmp/init.elementalx.rc /tmp/ramdisk/init.elementalx.rc
+chmod 0750 /tmp/ramdisk/init.elementalx.rc
+
+find . | cpio -o -H newc | gzip > /tmp/boot.img-ramdisk.gz
+rm -r /tmp/ramdisk
