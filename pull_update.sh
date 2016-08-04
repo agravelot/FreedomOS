9d96c58d2badeff64d0de0655527feec06dd0854
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-05 00:02:33 +0200

    pull_update support miltiple folder version "-2,-3,.."
    Now the script can pull on folder with:
    xxxx-1
    xxxx-2

--- pull_update.sh
+++ pull_update.sh
@@ -6,42 +6,39 @@ adb shell "mount /system"
 DATA_PATH="data/app/"
 
 app_list="
-com.google.android.apps.photos-1
-com.google.android.apps.maps-1
-com.google.android.calendar-1
-com.google.android.apps.messaging-1
-com.google.android.youtube-1
-com.google.android.music-1
-com.google.android.talk-1
-com.android.chrome-1
-com.touchtype.swiftkey-1
-com.google.android.gm-1
-com.google.android.apps.docs-1
-com.google.android.googlequicksearchbox-1
-com.google.android.music-1
-de.robv.android.xposed.installer-1
+com.google.android.apps.photos-
+com.google.android.apps.maps-
+com.google.android.calendar-
+com.google.android.apps.messaging-
+com.google.android.youtube-
+com.google.android.music-
+com.google.android.talk-
+com.android.chrome-
+com.touchtype.swiftkey-
+com.google.android.gm-
+com.google.android.apps.docs-
+com.google.android.googlequicksearchbox-
+com.google.android.music-
+de.robv.android.xposed.installer-
 "
 
 for i in ${app_list}
 do
-  mkdir -p ${DATA_PATH}${i}
+  mkdir -p ${DATA_PATH}${i}1
 done
 
-for i in ${app_list}
+for f in ${app_list}
 do
   echo " "
-  echo "Pulling ${i} :"
-  adb pull /data/app/${i} ${DATA_PATH}${i}
+  #cd "${f}1"
+  echo "Pulling ${f} :"
+  x=$(adb shell 'ls /data/app/' | grep "${f}" | tr '\r' ' ')
+  adb pull /data/app/${x} data/app/${f}1
+  #cd ..
 done
 
-cd ${DATA_PATH}
-chmod 775 *
-cd -
+chmod 775 ${DATA_PATH}*
 
-echo "Remove .odex files"
-cd ${DATA_PATH}
-find . -name "*.odex" -type f -delete
-cd -
-
+echo " "
 echo "Pull hosts file"
 adb pull /system/etc/hosts tools/adaway/hosts
