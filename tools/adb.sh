95b3b7b3bc1713590463b28edfa0604d97b44570
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-05 10:08:17 +0200

    Remove busybox sh script

--- tools/adb.sh
+++ tools/adb.sh
@@ -2,10 +2,9 @@
 #
 # Author : Nevax
 #
-cd /
-busybox echo "persist.service.adb.enable=1" >> /default.prop
-busybox echo "persist.service.debuggable=1" >> /default.prop
-busybox echo "persist.sys.usb.config=mtp,adb" >> /default.prop
-busybox echo "persist.service.adb.enable=1" >> /system/build.prop
-busybox echo "persist.service.debuggable=1" >> /system/build.prop
-busybox echo "persist.sys.usb.config=mtp,adb" >> /system/build.prop
+echo "persist.service.adb.enable=1" >> /default.prop
+echo "persist.service.debuggable=1" >> /default.prop
+echo "persist.sys.usb.config=mtp,adb" >> /default.prop
+echo "persist.service.adb.enable=1" >> /system/build.prop
+echo "persist.service.debuggable=1" >> /system/build.prop
+echo "persist.sys.usb.config=mtp,adb" >> /system/build.prop
--- tools/google_dns.sh
+++ tools/google_dns.sh
@@ -2,5 +2,5 @@
 #
 # Author : Nevax
 #
-busybox echo "net.dns1=8.8.8.8" >> /system/build.prop
-busybox echo "net.dns2=8.8.4.4" >> /system/build.prop
+echo "net.dns1=8.8.8.8" >> /system/build.prop
+echo "net.dns2=8.8.4.4" >> /system/build.prop
