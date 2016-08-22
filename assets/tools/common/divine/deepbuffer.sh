90e447134d582ea232012bdb9f17c92b84fe2358
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-22 15:53:26 +0200

    Update divine beats 7.5 beta1
      UPDATE: New and updated Huawei P9 libs.
      UPDATE: New vivo X-Play libs.
      UPDATE: LG G5 libs included.
      UPDATE: SRS optimized.
      UPDATE: Fully optimized Sony DSEE HX .
      UPDATE: Removed useless files from Divine Beats. This makes our mod slimmer.
      FIX: Fixed Bluetooth problem.
      FIX: Tried to fix the cracking sound some users were facing.
      FIX: Fix sound issue with soundcloud and some others apps.

--- assets/tools/common/divine/deepbuffer.sh
+++ assets/tools/common/divine/deepbuffer.sh
@@ -1,3 +1,9 @@
 #!/sbin/sh
 
-busybox sed -i '/deep_buffer {/,/}/s/^/#/' /system/etc/audio_policy.conf
+if [ "`grep ro.product.brand=Sony /system/build.prop`" ];
+then
+else
+ busybox cp -f /customize/dseehx/lib/libsonydseehxwrapper.so /system/lib/libsonydseehxwrapper.so
+fi
+
+sed -i '/deep_buffer {/,/}/d' /system/etc/audio_policy.conf
