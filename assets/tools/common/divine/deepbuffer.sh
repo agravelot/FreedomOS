79afb37ea2af489ca7ff2de5903ee7579e4f90de
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-22 15:53:26 +0200

    Revert "Update divine beats 7.5 beta1"
    This reverts commit 90e447134d582ea232012bdb9f17c92b84fe2358.

--- assets/tools/common/divine/deepbuffer.sh
+++ assets/tools/common/divine/deepbuffer.sh
@@ -1,9 +1,3 @@
 #!/sbin/sh
 
-if [ "`grep ro.product.brand=Sony /system/build.prop`" ];
-then
-else
- busybox cp -f /customize/dseehx/lib/libsonydseehxwrapper.so /system/lib/libsonydseehxwrapper.so
-fi
-
-sed -i '/deep_buffer {/,/}/d' /system/etc/audio_policy.conf
+busybox sed -i '/deep_buffer {/,/}/s/^/#/' /system/etc/audio_policy.conf
--- assets/tools/common/divine/runme.sh
+++ assets/tools/common/divine/runme.sh
@@ -702,7 +702,5 @@ then
 	# Add effect
 	sed -i 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' $CONFIG_FILE
 
-#!/sbin/sh
 
-sed -i '/deep_buffer {/,/}/d' /system/etc/audio_policy.conf
 
