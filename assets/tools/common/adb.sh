d79a8a00cee4c50fa65ba605b34351943bd31004
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-24 12:19:58 +0200

    remove adb on default.prop, it wil be set by future the kernel patch
    This file is in the ramdisk and can't be edited like that.

--- assets/tools/common/adb.sh
+++ assets/tools/common/adb.sh
@@ -2,9 +2,6 @@
 #
 # Author : Nevax
 #
-echo "persist.service.adb.enable=1" >> /default.prop
-echo "persist.service.debuggable=1" >> /default.prop
-echo "persist.sys.usb.config=mtp,adb" >> /default.prop
 echo "persist.service.adb.enable=1" >> /system/build.prop
 echo "persist.service.debuggable=1" >> /system/build.prop
 echo "persist.sys.usb.config=mtp,adb" >> /system/build.prop
