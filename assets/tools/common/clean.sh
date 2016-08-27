1fd96c9a39080442138e21a27b713c87a9076956
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-08-27 13:59:38 +0200

    Uninstall the data app only if selected
    Before this commit, the installer remove all the gapps provided by the rom in the clean script.
    Now the installer will remove the data apps folder, only iof it was selected.
    Like that the user update in the data partition stay in place.

--- assets/tools/common/clean.sh
+++ assets/tools/common/clean.sh
@@ -2,21 +2,6 @@
 #
 # Author : Nevax
 #
-echo "DEBUG: Remove conflits data app"
-rm -R /data/app/com.android.chrome-*
-rm -R /data/app/com.google.android.apps.docs-*
-rm -R /data/app/com.google.android.apps.maps-*
-rm -R /data/app/com.google.android.apps.messaging-*
-rm -R /data/app/com.google.android.apps.photos-*
-rm -R /data/app/com.google.android.calendar-*
-rm -R /data/app/com.google.android.gm-*
-rm -R /data/app/com.google.android.googlequicksearchbox-*
-rm -R /data/app/com.google.android.music-*
-rm -R /data/app/com.google.android.talk-*
-rm -R /data/app/com.google.android.youtube-*
-rm -R /data/app/com.touchtype.swiftkey-*
-rm -R /data/app/de.robv.android.xposed.installer-*
-
 echo "DEBUG: Set sdcard perm"
 #chown -R sdcard_rw:sdcard_rw /sdcard/
 chown -R media_rw:media_rw /data/media
