81bc8367ceecb2e4ee61c5e82f2f55d0044cff1b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-09-03 12:09:03 +0200

    remove bak file with sed commands
    The features is needed on macOS, Linux system don't need that.

--- build/scripts/add_files.sh
+++ build/scripts/add_files.sh
@@ -27,30 +27,27 @@ function add_files {
   cp -v ${assets_root}/media/wallpaper/* ${tmp_root}/media/wallpaper >> ${build_log} 2>&1
 
   echo ">> Set Assert in updater-script" 2>&1 | tee -a ${build_log}
-  sed -i.bak "s:!assert!:$ASSERT:" ${tmp_root}/META-INF/com/google/android/updater-script >> ${build_log} 2>&1
+  sed -i "s:!assert!:$ASSERT:" ${tmp_root}/META-INF/com/google/android/updater-script >> ${build_log} 2>&1
 
   if [ ! -z $ASSERT_2 ];
   then
     echo ">> Set Assert in updater-script" 2>&1 | tee -a ${build_log}
-    sed -i.bak "s:!assert2!:$ASSERT_2:" ${tmp_root}/META-INF/com/google/android/updater-script >> ${build_log} 2>&1
+    sed -i "s:!assert2!:$ASSERT_2:" ${tmp_root}/META-INF/com/google/android/updater-script >> ${build_log} 2>&1
   fi
 
   echo ">> Set VERSION in aroma" 2>&1 | tee -a ${build_log}
-  sed -i.bak "s:!version!:$VERSION:" ${tmp_root}/META-INF/com/google/android/aroma-config >> ${build_log} 2>&1
+  sed -i "s:!version!:$VERSION:" ${tmp_root}/META-INF/com/google/android/aroma-config >> ${build_log} 2>&1
 
   echo ">> Set device in aroma" 2>&1 | tee -a ${build_log}
-  sed -i.bak "s:!device!:${device}:" ${tmp_root}/META-INF/com/google/android/aroma-config >> ${build_log} 2>&1
+  sed -i "s:!device!:${device}:" ${tmp_root}/META-INF/com/google/android/aroma-config >> ${build_log} 2>&1
 
   echo ">> Set date in aroma" 2>&1 | tee -a ${build_log}
-  sed -i.bak "s:!date!:$(date +"%d%m%y"):" ${tmp_root}/META-INF/com/google/android/aroma-config >> ${build_log} 2>&1
+  sed -i "s:!date!:$(date +"%d%m%y"):" ${tmp_root}/META-INF/com/google/android/aroma-config >> ${build_log} 2>&1
 
   echo ">> Set date in en.lang" 2>&1 | tee -a ${build_log}
-  sed -i.bak "s:!date!:$(date +"%d%m%y"):" ${tmp_root}/META-INF/com/google/android/aroma/langs/en.lang >> ${build_log} 2>&1
+  sed -i "s:!date!:$(date +"%d%m%y"):" ${tmp_root}/META-INF/com/google/android/aroma/langs/en.lang >> ${build_log} 2>&1
 
   echo ">> Set date in fr.lang" 2>&1 | tee -a ${build_log}
-  sed -i.bak "s:!date!:$(date +"%d%m%y"):" ${tmp_root}/META-INF/com/google/android/aroma/langs/fr.lang >> ${build_log} 2>&1
-
-  rm -rvf ${tmp_root}/META-INF/com/google/android/aroma-config.bak >> ${build_log} 2>&1
-  rm -rvf ${tmp_root}/META-INF/com/google/android/aroma/langs/*.lang.bak >> ${build_log} 2>&1
+  sed -i "s:!date!:$(date +"%d%m%y"):" ${tmp_root}/META-INF/com/google/android/aroma/langs/fr.lang >> ${build_log} 2>&1
 
 }
