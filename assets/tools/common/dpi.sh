85b7611904de0730eafb537d51ad466e89fce1b0
NevaX <antoine.gravelot@hotmail.fr>
2016-10-08 19:51:26 +0200

    all: set dpi in during build
    The dpi will be set at the default value during the build process, and modified by the rom installer.

--- assets/tools/common/dpi.sh
+++ assets/tools/common/dpi.sh
@@ -3,4 +3,12 @@
 #	Author : Nevax
 #
 /tmp/tools/busybox echo "Set DPI to $1"
-/tmp/tools/busybox echo ro.sf.lcd_density=$1 >> /system/build.prop
+dpi=$1
+
+if grep -qr ro.sf.lcd_density= /system/build.prop ; then
+  /tmp/tools/busybox echo "DEBUG: ro.sf.lcd_density is set, add value $dpi"
+  /tmp/tools/busybox sed -i "s/ro.sf.lcd_density=.*/ro.sf.lcd_density=$dpi/" /system/build.prop
+else
+  /tmp/tools/busybox echo "DEBUG: ro.sf.lcd_density is not set, change value $dpi"
+  /tmp/tools/busybox echo ro.sf.lcd_density=$dpi >> /system/build.prop
+fi
