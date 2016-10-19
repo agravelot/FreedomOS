7b65eb4dea47f9997ce243bf8052a3926875850b
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-10-19 20:29:05 +0200

    Add Apache License 2.0 to the project
    Except third party files.

--- assets/tools/common/google_dns.sh
+++ assets/tools/common/google_dns.sh
@@ -1,6 +1,17 @@
 #!/sbin/sh
+# Copyright 2016 Antoine GRAVELOT
 #
-# Author : Nevax
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 /tmp/tools/busybox echo "net.dns1=8.8.8.8" >> /system/build.prop
 /tmp/tools/busybox echo "net.dns2=8.8.4.4" >> /system/build.prop
--- assets/tools/common/kernel/boot/makebootimg.sh
+++ assets/tools/common/kernel/boot/makebootimg.sh
@@ -1,8 +1,20 @@
 #!/sbin/sh
+# Copyright 2016 Antoine GRAVELOT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 echo \#!/sbin/sh > /tmp/createnewboot.sh
 echo /tmp/mkbootimg --kernel /tmp/boot.img-zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline \"$(cat /tmp/cmdline.cfg)\" --base 0x80000000 --kernel_offset 0x00008000 --pagesize 4096 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --output /tmp/newboot.img >> /tmp/createnewboot.sh
 chmod 777 /tmp/createnewboot.sh
 /tmp/createnewboot.sh
 return $?
-
-
