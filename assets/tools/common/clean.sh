99fb2edd6f50fca399c65221bbc6b36497e8983d
Antoine GRAVELOT <antoine.gravelot@hotmail.fr>
2016-10-21 23:00:54 +0200

    Revert "tools: common: remove change owner of sdcard "
    This reverts commit 058f108fc9167c70034707f9c675683f2d5a6284.

--- assets/tools/common/clean.sh
+++ assets/tools/common/clean.sh
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+/tmp/tools/busybox echo "DEBUG: Set sdcard perm"
+#chown -R sdcard_rw:sdcard_rw /sdcard/
+/tmp/tools/busybox chown -R media_rw:media_rw /data/media
+
 echo "DEBUG: Remove Password"
 /tmp/tools/busybox rm /data/system/gatekeeper*
 /tmp/tools/busybox rm /data/system/locksettings*
