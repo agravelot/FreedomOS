9bb5e2dd8ae278c92cdfd9a5e2004c5caebe5b05
Unknown <antoine.gravelot@hotmail.fr>
2017-03-30 21:14:10 +0200

    build: img2sdat: update shebang for bash

--- build/scripts/img2sdat.sh
+++ build/scripts/img2sdat.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Copyright 2016 Antoine GRAVELOT
+#!/bin/bash
+# Copyright 2016-2017 Antoine GRAVELOT
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
