fce23b994f95e581b5ca7d59d8067b021cc489ac
Unknown <antoine.gravelot@hotmail.fr>
2017-04-19 01:38:30 +0200

    magisk-manager: only  install it if not already installed
    also change the path

--- assets/tools/common/magisk/install.sh
+++ assets/tools/common/magisk/install.sh
@@ -12,20 +12,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+APP_PATH="/data/app/com.topjohnwu.magisk-1"
+# Remove old conflict installation
+rm -r /data/app/Magisk
+
 echo "Installing Magisk apk"
 mount /data
 if [[ ! -d /data/app/ ]]; then
   mkdir -p /data/app/
 fi
-rm -rvf /data/app/com.topjohnwu.magisk-*
-cp -rvf /tmp/tools/magisk/Magisk /data/app/
-cd /data/app/Magisk
-chown 1000.1000 . -R
-chown 1000:1000 . -R
-chmod 755 -R .
-chmod 755 lib/
-find -type f -name '.apk' -exec chmod 644 {} \;
-find -type f -name '.so' -exec chmod 755 {} \;
-chcon -R -t apk_data_file /data/app/Magisk/
-cd -
+
+# If Magisk already installed in user apps
+ls /data/app | grep com.topjohnwu.magisk-
+if [ "$?" -eq "0" ];
+then
+  echo "Masgisk Manager installation detected!"
+  echo "Keep it in place"
+else
+  echo "No Magisk installation found, Installing..."
+  cp -rvf /tmp/tools/magisk/com.topjohnwu.magisk-1 /data/app/
+  chown 1000.1000 $APP_PATH -R
+  chown 1000:1000 $APP_PATH -R
+  chmod 755 -R $APP_PATH
+  chmod 755 $APP_PATH/lib/
+  find -$APP_PATH type f -name '.apk' -exec chmod 644 {} \;
+  find $APP_PATH -type f -name '.so' -exec chmod 755 {} \;
+  chcon -R -t apk_data_file $APP_PATH
+fi
 exit 0
