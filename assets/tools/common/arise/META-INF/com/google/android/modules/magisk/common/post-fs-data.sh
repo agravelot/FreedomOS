f29dff2d7bf336608e0abd0f889661aa9221ab29
Nevax <antoine.gravelot@hotmail.fr>
2017-06-22 21:16:53 +0200

    arise4magik: patch for magisk v13

--- assets/tools/common/arise/META-INF/com/google/android/modules/magisk/common/post-fs-data.sh
+++ assets/tools/common/arise/META-INF/com/google/android/modules/magisk/common/post-fs-data.sh
@@ -5,3 +5,6 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+/data/magisk/magisk sepolicy-inject --live "allow mediaserver mediaserver_tmpfs file { read write execute }" \
+"allow audioserver audioserver_tmpfs file { read write execute }"
