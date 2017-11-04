fd705655ca67ac2f7a7ea1be66b6f95ce04489d4
Unknown <antoine.gravelot@hotmail.fr>
2017-11-04 01:19:24 +0100

    Update arise opus 20170811

--- assets/tools/common/arise/META-INF/com/google/android/modules/magisk/common/service.sh
+++ assets/tools/common/arise/META-INF/com/google/android/modules/magisk/common/service.sh
@@ -6,9 +6,6 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-/data/magisk/magisk sepolicy-inject --live "allow mediaserver mediaserver_tmpfs file { read write execute }" \
-"allow audioserver audioserver_tmpfs file { read write execute }"
-
 if [ -d "/system/system" ]; then
   SYS=/system/system
 else
